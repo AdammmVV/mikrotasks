3b426ff78dc8a6944d02150e6de9a8bd3a59d2c9
Adammm <adammm.web@gmail.com>
2023-01-07 12:41:28 +0300

    create Body.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./seit/Header";
+import {Header} from "./site/Header";
+import {Body} from "./site/Body";
 
 function App() {
-  return (
-      <Header />
-  )
+    return (
+        <>
+            <Header/>
+            <Body/>
+        </>
+
+    )
 }
 
 export default App;
