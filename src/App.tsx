b3cf9233ce525c90164a514479d222e7eb21e663
Adammm <adammm.web@gmail.com>
2023-01-07 12:37:49 +0300

    create Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./seit/Header";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+      <Header />
+  )
 }
 
 export default App;
