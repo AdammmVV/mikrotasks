d1e1d1ed02ad801fd390ddc5cfd9b3ad32d24a23
Adammm <adammm.web@gmail.com>
2023-01-07 16:44:01 +0300

    change App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React, {useState} from 'react';
 import './App.css';
+import {Money} from "./site/Money";
 // import {Header} from "./site/Header";
 // import {Body} from "./site/Body";
 // import {Footer} from "./site/Footer";
 // import {NewComponent} from "./site/NewComponent";
 // import {Button} from "./site/Button";
+
+export type FilterMoneyType = 'all' | 'dollars' | 'rubles';
 
 function App() {
 
@@ -32,16 +35,45 @@ function App() {
     // const Button1Foo = (subscriber: string = 'I`m stupid button', age: number = 0, address: string = '') => {
     //     console.log(subscriber, age, address)
     // }
+    //
+    // let [a, setA] = useState(1);
+    //
+    // const onClickHandler = () => {
+    //     setA(++a);
+    //     console.log(a)
+    // }
+    // const resetsList = () => {
+    //     setA(a = 0);
+    //     console.log(a)
+    // }
 
-    let [a, setA] = useState(1);
+    let [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
+        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
+        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
+    ])
 
-    const onClickHandler = () => {
-        setA(++a);
-        console.log(a)
+    let [filter, setFilter] = useState<FilterMoneyType>('all')
+
+    let currentMoney = money
+
+    const FilterMoney = (filter: FilterMoneyType) => {
+        return setFilter(filter);
     }
-    const resetsList = () => {
-        setA(a = 0);
-        console.log(a)
+    if(filter === 'rubles') {
+        currentMoney = money.filter(m => {
+            return m.banknote === 'ruble'
+        })
+    }
+    if(filter === 'dollars') {
+        currentMoney = money.filter(m => {
+            return m.banknote === 'dollar'
+        })
     }
 
     return (
@@ -53,9 +85,10 @@ function App() {
             {/*<Button name={'MyYouTubeChanel-1'} callBack={() => Button1Foo('I`m Vasya', 21, 'I`m from Minsk')}/>*/}
             {/*<Button name={'MyYouTubeChanel-2'} callBack={() => Button1Foo('I`m Ivan')}/>*/}
             {/*<Button name={'Stupid Button'} callBack={() => Button1Foo()}/>*/}
-            <h2>{a}</h2>
-            <button onClick={onClickHandler}>Counter</button>
-            <button onClick={resetsList}>0</button>
+            {/*<h2>{a}</h2>*/}
+            {/*<button onClick={onClickHandler}>Counter</button>*/}
+            {/*<button onClick={resetsList}>0</button>*/}
+            <Money money={currentMoney} FilterMoney={FilterMoney}/>
         </>
     )
 }
